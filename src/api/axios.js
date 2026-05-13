162168efcb498953f50c1cfc4a54ea336f245d3b
kcardonaquiroz <kcardonaquiroz@gmail.com>
2026-05-13 10:16:24 -0500

    Agregando api/v1 a la base URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://admin-cars-backend.onrender.com',
+  baseURL: 'https://admin-cars-backend.onrender.com/api/v1',
 })
 
 api.interceptors.request.use((config) => {
